097f08aadcd15623b12506db55f450b78767f17c
jamesjnadeau <james.nadeau@gmail.com>
2018-10-30 00:17:18 +0000

    :zap: add identity and check for it

--- functions/github.js
+++ functions/github.js
@@ -11,11 +11,15 @@ octokit.authenticate({
   token: process.env.GITHUB_OAUTH_TOKEN,
 });
 
+var handleError = function(err) {
+  console.error(err);
+};
+
 var updateContent = function(path, message, content, cb) {
   console.log({
-    owner: process.env.GITHUB_USERNAME,
-    repo: process.env.GITHUB_REPO_NAME,
     path,
+    message,
+    content
   });
   try {
     octokit.repos.getContent({
@@ -23,20 +27,19 @@ var updateContent = function(path, message, content, cb) {
       repo: process.env.GITHUB_REPO_NAME,
       path,
     }).then(function(origContent) {
-      console.log(origContent);
-    /*  octokit.repos.updateFile({
+      // console.log(origContent);
+      octokit.repos.updateFile({
         owner: process.env.GITHUB_USERNAME,
         repo: process.env.GITHUB_REPO_NAME,
         path,
         message, // The commit message
         content, // The new file content, using Base64 encoding.
-        sha: origContent.sha, // The blob SHA of the file being replaced.
+        sha: origContent.data.sha, // The blob SHA of the file being replaced.
       }).then(function(result) {
         console.log(result);
         cb();
-      });
-      */
-    });
+      }).catch(handleError);
+    }).catch(handleError);
   } catch (err) {
     console.error(err);
   }
@@ -44,15 +47,19 @@ var updateContent = function(path, message, content, cb) {
 };
 
 exports.handler = function(ev, context, callback) {
-  // console.log('event', ev);
-  // console.log('context', context);
+
+  if (!context || !context.clientContext || !context.clientContext.user) {
+    return callback(new Error('Not Authenticated'));
+  }
 
   var body = JSON.parse(ev.body);
   console.log('body', body);
 
-  var path = '/content' + body.path + 'index.html';
+  var path = 'content' + body.path + 'index.html';
+  var message = body.message || 'Test commit message';
+  var content = Buffer.from(body.content).toString('base64');
 
-  updateContent(path, function() {
+  updateContent(path, message, content, function() {
     callback(null, {
       // "isBase64Encoded": false,
       statusCode: 200,
