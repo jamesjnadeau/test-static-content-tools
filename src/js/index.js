b226240d8fbc2a8fb2618d3de58fd9102d84fb90
jamesjnadeau <james.nadeau@gmail.com>
2018-10-31 10:57:14 -0400

    :tada: initial work on doing git updates in browser through git gateway

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,45 @@
 require("bootstrap/dist/js/bootstrap.js");
 var ContentTools = require('ContentTools/build/content-tools.min.js')
 
+var handleError = function(err) {
+  console.error(err);
+};
+
+var updateContent = function(path, message, content, cb) {
+  var url = '/.netlify/git/github/contents/' + path;
+  $.ajax({
+    url: url,
+  }).then(function(origContent) {
+    $.ajax({
+      url: url,
+      type: 'PUT',
+      data: {
+        message: message,
+        content: content,
+        sha: origContent.data.sha,
+      }
+    }).then(function(result) {
+      console.log(result);
+      cb();
+    }).catch(handleError);
+    // console.log(origContent);
+  }).catch(handleError);
+};
+
 window.addEventListener('load', function() {
   var editor = ContentTools.EditorApp.get();
   editor.init('*[data-editable]', 'data-name');
 
-  editor.addEventListener('saved', function (ev) {
-    var name, payload, regions, xhr;
+  editor.addEventListener('saved', function(ev) {
+    var payload;
+    var regions;
+    var xhr;
 
     // Check that something changed
     regions = ev.detail().regions;
     console.log(regions);
-    if (Object.keys(regions).length == 0) {
-        return;
+    if (Object.keys(regions).length === 0) {
+      return;
     }
 
     // Set the editor as busy while we save our changes
@@ -20,18 +47,28 @@ window.addEventListener('load', function() {
 
     // Collect the contents of each region into a FormData instance
     payload = {};
-    for (name in regions) {
-        if (regions.hasOwnProperty(name)) {
-            payload[name] = regions[name];
-        }
-    }
+    var keys = Object.keys(regions);
+    keys.forEach(function(name) {
+      payload[name] = regions[name];
+    });
 
-    payload.path = location.pathname;
+    var myPath = window.location.pathname;
+    payload.path = 'content' + myPath;
+    if (myPath[myPath.length - 1] === '/') { // if last char is a '/'
+      payload.path += 'index;'
+    }
+    payload.path += '.html';
 
     // Debug payload
     console.log(payload);
 
 
+    updateContent(payload.path, 'Test JS based commit', payload.content, function() {
+      editor.busy(false);
+      new ContentTools.FlashUI('ok');
+    });
+
+    /*
     // Send the update content to the server to be saved
     function onStateChange(ev) {
         // Check if the request is finished
@@ -47,15 +84,12 @@ window.addEventListener('load', function() {
         }
     };
 
-    new ContentTools.FlashUI('ok');
-    editor.busy(false);
-
-
     xhr = new XMLHttpRequest();
     xhr.addEventListener('readystatechange', onStateChange);
-    xhr.open('POST', '/.netlify/functions/github');
+    xhr.open('POST', '/.netlify/git/github/contents/');
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.send(JSON.stringify(payload));
+    */
 });
 
 
