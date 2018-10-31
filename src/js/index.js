919a0d8e3dd82fe0ef5e13da14be8ae2bd839fe3
jamesjnadeau <james.nadeau@gmail.com>
2018-10-31 14:41:04 -0400

    :tada: add netlify-identity-widget package, use it

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,20 @@
 require("bootstrap/dist/js/bootstrap.js");
+
+var netlifyIdentity = require("netlify-identity-widget");
+
 var loadContentTools = require('./contentTools');
+
+netlifyIdentity.init();
 
 var currentUser = netlifyIdentity.currentUser();
 
 if (currentUser) {
   currentUser.jwt().then(function(accessToken) {
-    loadContentTools(accessToken)
+    loadContentTools(accessToken);
   });
 } else {
   netlifyIdentity.on('login', function(user) {
-    getPrivateContent(user.token.access_token);
+    loadContentTools(user.token.access_token);
     netlifyIdentity.close();
   });
 
