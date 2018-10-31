7c76bce7bae57fa2927da876bd5e18a67c77d133
jamesjnadeau <james.nadeau@gmail.com>
2018-10-31 14:45:20 -0400

    :memo: better logging on login

--- src/js/index.js
+++ src/js/index.js
@@ -7,15 +7,15 @@ var loadContentTools = require('./contentTools');
 netlifyIdentity.init();
 
 var currentUser = netlifyIdentity.currentUser();
-
+console.log('user', currentUser);
 if (currentUser) {
   currentUser.jwt().then(function(accessToken) {
     loadContentTools(accessToken);
   });
 } else {
   netlifyIdentity.on('login', function(user) {
-    loadContentTools(user.token.access_token);
     netlifyIdentity.close();
+    loadContentTools(user.token.access_token);
   });
 
   netlifyIdentity.open('login');
