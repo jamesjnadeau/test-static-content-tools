9e9b5d3607358558e757473e7ff39a86b7c78433
jamesjnadeau <james.nadeau@gmail.com>
2018-11-01 16:40:11 +0000

    :tada: move editor auth to it's own file

---
+++ src/js/auth.js
@@ -0,0 +1,20 @@
+var netlifyIdentity = require("netlify-identity-widget");
+
+var loadContentTools = require('./contentTools');
+
+netlifyIdentity.init();
+
+var currentUser = netlifyIdentity.currentUser();
+console.log('user', currentUser);
+if (currentUser) {
+  currentUser.jwt().then(function(accessToken) {
+    loadContentTools(accessToken);
+  });
+} else {
+  netlifyIdentity.on('login', function(user) {
+    netlifyIdentity.close();
+    loadContentTools(user.token.access_token);
+  });
+
+  netlifyIdentity.open('login');
+}
